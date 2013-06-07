222dd4384f551a9a4b27b3e72c1d31d61ea5a3b2
vc <vc@chupin.uvaresearch.com>
2013-06-07 15:17:26 +0700

    притащил пример на основе Stage

--- dots-android/src/net/slezok/dots/MainActivity.java
+++ dots-android/src/net/slezok/dots/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AndroidApplication {
         cfg.useGL20 = false;
         cfg.useAccelerometer = false;
         cfg.useCompass = false;
-        initialize(new Dots(), cfg);
+        initialize(new LibGdxGame(), cfg);
     }
 }
