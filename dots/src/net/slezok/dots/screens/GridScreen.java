db5846c436a03dc030912f2c570e3db8db09f399
vc <vc@chupin.uvaresearch.com>
2013-06-26 12:37:45 +0700

    раздвинул кнопки

--- dots/src/net/slezok/dots/screens/GridScreen.java
+++ dots/src/net/slezok/dots/screens/GridScreen.java
@@ -127,12 +127,12 @@ public class GridScreen implements Screen{
 		table.setFillParent(true);
 		table.defaults().width(100).height(80);
 		table.debug();
-		table.add(upButton).colspan(2).align(BaseTableLayout.CENTER);
+		table.add(upButton).padBottom(50).colspan(2).align(BaseTableLayout.CENTER);
 		table.row();
 		table.add(leftButton).padRight(100);
 		table.add(rightButton).padLeft(100);
 		table.row();
-		table.add(downButton).colspan(2).align(BaseTableLayout.CENTER);
+		table.add(downButton).padTop(50).colspan(2).align(BaseTableLayout.CENTER);
 		
 		Image bgrImage = new Image(Assets.backgroundTexture);
 		bgrImage.setFillParent(true);
