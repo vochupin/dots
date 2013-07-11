4b47912f93a6392ab8d147dc1a379628bcf02b43
vochupin <vochupin@gmail.com>
2013-07-11 13:34:41 +0700

    в андроиде для фоновой музыки открывается копия файла из каталога программы

--- graphdict-android/src/net/slezok/dots/MainActivity.java
+++ graphdict-android/src/net/slezok/dots/MainActivity.java
@@ -1,14 +1,31 @@
 package net.slezok.dots;
 
-import android.os.Bundle;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.os.Environment;
+
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.AndroidFileHandle;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.swarmconnect.Swarm;
 
 public class MainActivity extends AndroidApplication implements PlatformDependencies{
+	private static final String TAG = null;
+	private static final int IO_BUFFER_SIZE = 4 * 1024;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,6 +56,34 @@ public class MainActivity extends AndroidApplication implements PlatformDependen
 
 	@Override
 	public FileHandle openAssetFileCopyIfNeeded(String assetPath) {
-		return Gdx.files.internal(assetPath);
+		InputStream is = null; OutputStream os = null;
+		try {
+			AssetManager am = getAssets();
+
+			AssetFileDescriptor afd = am.openFd(assetPath);
+			File copyFile = new File(getFilesDir().getAbsolutePath() + "/" + new File(assetPath).getName());
+
+			if(!copyFile.exists() || copyFile.length() != afd.getLength()){
+				if(!copyFile.createNewFile()) throw new GdxRuntimeException("Can't create asset file copy: " + assetPath);
+				os = new BufferedOutputStream(new FileOutputStream(copyFile));
+				is = am.open(assetPath);
+				copy(is, os);
+				is.close();
+				os.close();
+			}
+			return Gdx.files.absolute(copyFile.getAbsolutePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new GdxRuntimeException("Exception in openAssetFileCopyIfNeeded: " + e.getMessage());
+		}
 	}
+
+	private static void copy(InputStream in, OutputStream out) throws IOException {  
+		byte[] b = new byte[IO_BUFFER_SIZE];  
+		int read;  
+		while ((read = in.read(b)) != -1) {  
+			out.write(b, 0, read);  
+		}
+		out.flush();
+	}  
 }
